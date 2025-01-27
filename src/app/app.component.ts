9ae83e448e841ea9d84ec112f1d409d3244a646c
mohamadjavad abolhasanifar <m.abolhasanifar@UGYM.LOCAL>
2025-01-27 14:43:52 +0330

    C

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { RestaurantCardComponent } from './restaurantCards/restaurantCard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, RestaurantCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'test-angular';
---
+++ src/app/restaurantCards/restaurantCard.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-restaurantCard',
+  templateUrl: './restaurantCard.component.html',
+  styleUrls: ['./restaurantCard.component.css'],
+})
+export class RestaurantCardComponent {}
