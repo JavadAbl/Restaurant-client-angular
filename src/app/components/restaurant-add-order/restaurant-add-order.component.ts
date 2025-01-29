41d59038fd0dc156897816c8a1665076f735adff
mohamadjavad abolhasanifar <m.abolhasanifar@UGYM.LOCAL>
2025-01-29 15:50:09 +0330

    C

---
+++ src/app/components/restaurant-add-order/restaurant-add-order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-add-order',
+  imports: [],
+  templateUrl: './restaurant-add-order.component.html',
+  styleUrl: './restaurant-add-order.component.css'
+})
+export class RestaurantAddOrderComponent {
+
+}
--- src/app/components/restaurant-dishes/restaurant-dishes.component.ts
+++ src/app/components/restaurant-dishes/restaurant-dishes.component.ts
@@ -1,12 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { IRestaurant } from '../../models/restaurant';
+import { RestaurantAddOrderComponent } from '../restaurant-add-order/restaurant-add-order.component';
 
 @Component({
   selector: 'app-restaurant-dishes',
-  imports: [],
+  imports: [RestaurantAddOrderComponent],
   templateUrl: './restaurant-dishes.component.html',
   styleUrl: './restaurant-dishes.component.css',
 })
 export class RestaurantDishesComponent {
   restaurant = input.required<IRestaurant | null>();
+  isShowOrder = signal(false);
+
+  handleShowOrder() {
+    this.isShowOrder.set(true);
+  }
+  handleHideOrder() {
+    this.isShowOrder.set(false);
+  }
 }
