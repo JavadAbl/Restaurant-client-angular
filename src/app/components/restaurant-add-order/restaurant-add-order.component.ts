d1f1bc09a36f4ecd4ec8acc5f3f9879ec4c9e4d3
Javad <com.javadabl@gmail.com>
2025-01-31 22:47:49 +0330

    C

--- src/app/components/restaurant-add-order/restaurant-add-order.component.ts
+++ src/app/components/restaurant-add-order/restaurant-add-order.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurant-add-order',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './restaurant-add-order.component.html',
-  styleUrl: './restaurant-add-order.component.css'
+  styleUrl: './restaurant-add-order.component.css',
 })
 export class RestaurantAddOrderComponent {
+  isShow = input.required<boolean>();
+  onExit = output<boolean>();
+  title = signal('');
+  order = signal('');
+  date = new Date();
 
+  test() {
+    console.log(this.title);
+  }
 }
